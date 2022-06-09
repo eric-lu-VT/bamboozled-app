3ee5e671946f672d15213b91479064ae0ee5b109
11Some1 <68611574+11Some1@users.noreply.github.com>
2022-06-09 02:06:23 -0400

    add websockets

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import { Provider } from 'react-redux';
 
 import store from './src/store';
 import AppNavigator from './src/navigation'
+import { socket, SocketContext } from './src/utils/socket';
+
 export default function App() {
   return (
     <Provider store={store}>
-      <AppNavigator />
+      <SocketContext.Provider value={socket}>
+        <AppNavigator />
+      </SocketContext.Provider> 
     </Provider>
   );
 }
